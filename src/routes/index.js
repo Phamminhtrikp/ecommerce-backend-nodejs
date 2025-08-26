3cbda637bce2f93b8f26b527b0f09dec75d9dc8c
Phamminhtrikp <tripham.210504@gmail.com>
2025-08-26 11:49:37 +0700

    Implement API key authentication and permissions management

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const express = require('express');
+const { apiKey, permission } = require('../auth/checkAuth');
 const router = express.Router();
 
-router.use('/v1/api', require('./access'));
-// router.get('', (req, res, next) => {
+// Check apiKey
+router.use(apiKey);
+// Check permission
+router.use(permission('0000'))
 
-//     return res.status(200).json({ 
-//         message: 'Welcome to the e-commerce API',
-//     });
-// });
+router.use('/v1/api', require('./access'));
 
 module.exports = router;
